fbdf63e9e5dfcfe4dc4ba5fbe675f6170a2702ac
ferdous <papiaferdous43@gmail.com>
2023-09-21 23:24:50 +0600

    carrer hub website with router

---
+++ src/Components/Banana/Banana.jsx
@@ -0,0 +1,10 @@
+
+const Banana = () => {
+    return (
+        <div>
+            <h3>Home page banner.</h3>
+        </div>
+    );
+};
+
+export default Banana;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banana from "../Banana/Banana";
+
+const Home = () => {
+    return (
+        <div>
+            <Banana></Banana>
+            <h2>This is a home.</h2>
+        </div>
+    );
+};
+
+export default Home;
